c07354937a9cdaca8ec354a2da268f3a27be71fd
defcdgg <83417124+defcdgg@users.noreply.github.com>
2026-03-17 12:14:16 +0800

    Match func sub_800096C
    Co-authored-by: defcdgg <83417124+defcdgg@users.noreply.github.com>
    Co-authored-by: testyourmine <testyourmine@users.noreply.github.com>

--- src/code_80002A0.c
+++ src/code_80002A0.c
@@ -152,3 +152,160 @@ void sub_80008CC(void) {
     VBlankIntrWait();
     REG_DISPCNT |= 0x80;
 }
+
+
+s32 sub_800096C(void) {
+
+    switch (gUnk_03001B50) 
+    {
+        case 0:
+            REG_RCNT = 0xC000;
+            VBlankIntrWait();
+            sub_8017600(&gUnk_03001AD0, &gUnk_03001CB0, 0x10, 0);
+            gUnk_03004F20.unk5E = 1;
+            gUnk_03004F20.unk2 = 1;
+            gUnk_03001B50++;
+        case 1:
+            gUnk_03001940 = sub_80171E4();
+            
+            if (gUnk_03001940 == -1) 
+            {
+                gUnk_030025A8 = 5;
+                gUnk_03001B50 = 6;
+                break;
+            }
+            
+            if ((sub_8017588() == 0) && ( gUnk_03001940 <= 0xFF))
+            {
+                if (gUnk_03001D30 == 0) {
+                    sub_801761C();
+                    gUnk_03001B50 = 4;
+                    gUnk_030025A8 = 2;
+                }
+                gUnk_03001D30--;
+                break;
+            }
+
+            switch (gUnk_03001940)
+            {
+                case 0:
+                case 1:
+                    if (gUnk_03001D30 == 0) 
+                    {
+                        sub_801761C();
+                        gUnk_03001B50 = 4;
+                        gUnk_030025A8 = 2;
+                    }
+                    else 
+                    {
+                        gUnk_03001D30--;
+                    }
+                    break;
+                
+                case 2:
+                    if (sub_8017588() == 0) 
+                    {
+                        sub_801761C();
+                        gUnk_03001B50 = 5;
+                        gUnk_030025A8 = 3;
+                    }
+                    else if (gUnk_030025A8 == 0) 
+                    {
+                        gUnk_03004F20.unk5E = 1;
+                        gUnk_03004F20.unk2 = 1;
+                        gUnk_030025A8 = 1;
+                    }
+                    break;
+                
+                case 0x101:
+                case 0x102:
+                case 0x103:
+                case 0x104:
+                    gUnk_030025A8 = 3;
+                    gUnk_03001B50 = 5;
+                    break;
+                
+                default:
+                break;
+            }
+
+            break;
+        case 2:
+        case 3:
+        case 4:
+            sub_801761C();
+            gUnk_03001B50 = 9;
+            break;
+        case 5:
+            sub_801761C();
+            gUnk_03001B50 = 9;
+            break;
+        case 6:
+            gUnk_03001B50 = 0;
+            break;
+        case 7:
+            sub_801761C();
+            break;
+        case 8:
+            if (gUnk_030025A8 == 0) 
+            {
+                sub_801761C();
+                gUnk_03001B50 = 0;
+            }
+            break;
+        case 9:
+            sub_801761C();
+            gUnk_03001B50 = 8;
+            break;
+    }
+    
+    return sub_8017588();
+}
+
+
+void sub_8000B58(void) {
+    u16 i;
+
+    DmaFill16(3, 0, (void*)VRAM, VRAM_SIZE);
+    DmaFill32(3, 0, (void*)OAM, OAM_SIZE);
+    DmaFill16(3, 0, (void*)PLTT, PLTT_SIZE);
+    DmaFill16(3, 0, &gUnk_02004000, 0x2000);
+    
+    sub_8004B8C();
+    sub_8004BBC();
+
+    gUnk_03001948 = 0;
+    gUnk_030032D0 = 0;
+    gUnk_03002604 = 0;
+    gUnk_03004910 = 0;
+    gUnk_03004840 = 0;
+    gUnk_0300465C = 0;
+    gUnk_030025F4 = 0xFF;
+
+
+    for (i = 0; i < 16; i++)
+    {
+        gUnk_03004800[i] = 0;
+    }
+    
+    for (i = 0; i < 128; i++)
+    {
+        gUnk_030035C0[i].all.unk0 = 0;
+        gUnk_030035C0[i].all.unk4 = 0;
+    }
+
+    sub_8004AC0();
+    sub_8004B60();
+    sub_8009A5C();
+    sub_8001090();
+    sub_8001014();
+    sub_8009168();
+    
+    gUnk_0300259C = 0;
+    gUnk_03004D4C = 0;
+    gUnk_03004D40 = 0;
+    gUnk_03004854 = 0;
+    gUnk_030025D8 = 0;
+    gUnk_0300483C = 0;
+    gUnk_03002C48 = 0;
+}
